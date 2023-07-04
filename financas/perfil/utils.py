a7a0f924656ade1fcb10b0e920443d11964aeb82
JoseAlexandreBarros <josealexandrebarros1990@gmail.com>
2023-07-04 17:34:12 -0300

    site django primeira versao

---
+++ financas/perfil/utils.py
@@ -0,0 +1,5 @@
+def calculo_total(obj, campo):
+    total=0
+    for i in obj:
+        total+=getattr(i,campo)
+    return total
---
+++ financas/perfil/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Categoria, Conta
+from django.contrib import messages
+from django.contrib.messages import constants
+from .utils import calculo_total
+
+
+
+
+def home(request):
+    contas=Conta.objects.all()
+    total_contas = calculo_total(contas,'valor')
+
+    
+    return render(request, 'home.html',{'contas':contas,'total_contas':total_contas})
+
+def gerenciar(request):
+    contas=Conta.objects.all()
+    categorias=Categoria.objects.all()
+    total_contas = 0
+
+    for conta in contas:
+        total_contas += conta.valor
+    
+    return render(request,'gerenciar.html',{'contas':contas,'categorias':categorias,'total_contas':total_contas})
+
+def cadastrar_banco(request):
+    
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.FILES.get('icone')
+    
+    if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
+        messages.add_message(request, constants.ERROR,('Preencha corretamente'))
+        return redirect('/perfil/gerenciar/')
+    
+    conta = Conta(
+        apelido = apelido,
+        banco=banco,
+        tipo=tipo,
+        valor=valor,
+        icone=icone
+    )
+
+    conta.save()
+    
+    messages.add_message(request, constants.SUCCESS,('Preenchido corretamente'))
+    return redirect('/perfil/gerenciar/')
+
+def deletar_banco(request,id):
+    
+    conta=Conta.objects.get(id=id)
+    conta.delete()
+    messages.add_message(request, constants.SUCCESS,('Conta deletada'))
+    return redirect('/perfil/gerenciar/')
+
+def cadastrar_categoria(request):
+    nome = request.POST.get('categoria')
+    essencial = bool(request.POST.get('essencial'))
+
+    categoria = Categoria(
+        categoria=nome,
+        essencial=essencial
+    )
+
+    categoria.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso')
+    return redirect('/perfil/gerenciar/')
+
+def update_categoria(request, id):
+    categoria = Categoria.objects.get(id=id)
+
+    categoria.essencial = not categoria.essencial
+
+    categoria.save()
+
+    return redirect('/perfil/gerenciar/')
